9734e1f30ee487e7775ac295161707b513b3b042
mcdon0619 <mcdon0619@gmail.com>
2022-11-09 10:54:34 -0500

    Found all unique crime types for each city and put it in a csv

---
+++ findCityCrimeTypes.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import pandas as pd
+from datetime import datetime
+import os
+
+directory = './CityData'
+
+files = Path(directory).glob('*')
+newDataFrame = pd.DataFrame()
+for file in files:
+    print("Finding crime types in " + file.name)
+    try:
+        data = pd.read_csv(file)
+        crimeTypes = data["offense"].unique()
+        tempDataFrame = pd.DataFrame()
+        tempDataFrame[file.name.split('_')[0]] = crimeTypes
+        newDataFrame = pd.concat([newDataFrame, tempDataFrame], axis=1)
+    except Exception as e:
+        print("Unable to find crime types in " + file.name +
+              ". This was the resulting error message: \n" + str(e))
+        continue
+
+newDataFrame.to_csv('./uniqueCrimeTypes.csv', index=False)
