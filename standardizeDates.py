2ad4731f9d8751921ba187616be5d61713506072
GrantFecteau <grant.pf@gmail.com>
2022-12-02 00:26:28 -0500

    update dates for Washington

--- standardizeDates.py
+++ standardizeDates.py
@@ -10,13 +10,26 @@ directory = './CityData'
 files = Path(directory).glob('*')
 for file in files:
     print("Reformatting dates in " + file.name)
-    try:
-        data = pd.read_csv(file)
-        data["date"] = pd.to_datetime(data["date"])
-        data["date"] = data['date'].dt.strftime('%m/%d/%Y')
-        os.remove("./CityData/"+file.name)
-        data.to_csv("./CityData/"+file.name, index=False)
-    except Exception as e:
-        print("Unable to reformat dates in " + file.name +
-              ". This was the resulting error message: \n" + str(e))
-        continue
+    if file.name == "Washington D.C._data.csv" :
+        try:
+            data = pd.read_csv(file)
+            data["0"] = pd.to_datetime(data["0"])
+            data["0"] = data["0"].dt.strftime('%m/%d/%Y')
+            os.remove("./CityData/"+file.name)
+            data.to_csv("./CityData/"+file.name, index=False)
+        except Exception as e:
+            print("Unable to reformat dates in " + file.name +
+                ". This was the resulting error message: \n" + str(e))
+            continue
+    else:
+        try:
+            data = pd.read_csv(file)
+            data["date"] = pd.to_datetime(data["date"])
+            data["date"] = data['date'].dt.strftime('%m/%d/%Y')
+            os.remove("./CityData/"+file.name)
+            data.to_csv("./CityData/"+file.name, index=False)
+        except Exception as e:
+            print("Unable to reformat dates in " + file.name +
+                ". This was the resulting error message: \n" + str(e))
+            continue
+    
