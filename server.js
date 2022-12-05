62a5c0383501384bf444c661ba0c9c6dc840a475
mcdon0619 <mcdon0619@gmail.com>
2022-12-04 22:57:12 -0500

    Dropdowns are now populated from Db

--- server.js
+++ server.js
@@ -2,46 +2,75 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+//setting view engine to ejs
+app.set("view engine", "ejs");
 
-app.use(express.json())
-app.use(express.static("html"));
+var con = mysql.createConnection({
+  host: "seniordesign-db.cxwhjsfccgui.us-east-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "password!",
+});
+
+//route for index page
+app.get("/", function (req, res) {
+  con.query(
+    "SELECT City FROM SeniorDesign.CrimeData GROUP BY City",
+    function (err, cities) {
+      if (err) {
+        req.flash("error", err);
+        res.render("index", { cities: "", years: "" });
+      } else {
+        con.query(
+          "SELECT Year(Date) AS Year FROM SeniorDesign.CrimeData GROUP BY Year(Date)",
+          function (err, years) {
+            if (err) {
+              req.flash("error", err);
+              res.render("index", { cities: "", years: "" });
+            } else {
+              res.render("index", { cities: cities, years: years });
+            }
+          }
+        );
+      }
+    }
+  );
+});
 
 // Post request - Calls the DB and returns all the results for a city within the given Year
-app.post('/getData', function (req, res) {
-    var city = req.body.City;
-    var year = req.body.Year;
+app.post("/getData", function (req, res) {
+  var city = req.body.City;
+  var year = req.body.Year;
 
-	// Config your database credential
-	var con = mysql.createConnection({
-        host: "seniordesign-db.cxwhjsfccgui.us-east-1.rds.amazonaws.com",
-        port: "3306",
-        user: "admin",
-        password: "password!"
-      });
+  // Config your database credential
 
-    var minDate = year+"0101";
-    var maxDate;
+  var minDate = year + "0101";
+  var maxDate;
 
-    if(year == 2019){
-        maxDate = "20200101";
-    } else {
-        maxDate = (parseInt(year)+1)+"0101";
-    }
+  if (year == 2019) {
+    maxDate = "20200101";
+  } else {
+    maxDate = parseInt(year) + 1 + "0101";
+  }
 
-    var query = 'SELECT * FROM SeniorDesign.CrimeData WHERE City="' + city + '" AND Date >= ' + minDate + ' AND Date < ' + maxDate + ';'
+  var query =
+    'SELECT * FROM SeniorDesign.CrimeData WHERE City="' +
+    city +
+    '" AND Date >= ' +
+    minDate +
+    " AND Date < " +
+    maxDate +
+    ";";
 
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        con.query(query, function (err, result) {
-            if (err) throw err;
-            res.send(result);
-            con.end();
-        });
-    });
-
+  con.query(query, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 var server = app.listen(5000, function () {
-	console.log('Server is listening at port 5000...');
+  console.log(__dirname + "/public");
+  console.log("Server is listening at port 5000...");
 });
